6e5b30a3fab2dbff4d432f475f887a71577d10ff
cms <daumtto53@gmail.com>
2025-01-18 19:11:19 +0900

    emmiting data

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+export @Component ({
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+})
+
+class HeaderComponent { }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output, } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required: true})  name!: string;
+  @Input({required: true})  avatar!: string;
+  @Input({required: true})  id!: string;
+
+  @Output() select = new EventEmitter();
+
+  get imagePath () {
+    return 'assets/users/' + this.avatar;
+  }
+
+  public onSelectUser () {
+    this.select.emit();
+   }
+
+}
