3522ce67cb30c249d23243eb0ebe4d9ca2e7466b
daumtto53 <daumtto53@gmail.com>
2025-01-19 00:40:53 +0900

    added dummy tasks with individual task.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,25 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { NgFor, } from '@angular/common';
+import { DUMMYTASKS } from './../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
-  templateUrl: './tasks.component.html',
+  imports: [TaskComponent, NgFor],
+templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input()  selectedName?: string;
+  @Input({required: true})  name!: string;
+  @Input({required: true})  userId!:   string;
+
+  tasks = DUMMYTASKS;
+
+  get selectedUser() {
+    return this.tasks.filter((entry) => entry.userId === this.userId)
+  }
+
+
 
 }
