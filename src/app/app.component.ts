ccaebf40842be1ee43e09360b702d3012b52deee
cms <daumtto53@gmail.com>
2025-01-18 22:18:45 +0900

    outputting list content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,27 @@ import {HeaderComponent} from './header/header.component';
 import { UserComponent } from './user/user.component';
 
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedId?: string;
+
+  get selectedUser() {
+    return this.users.find((element) => element.id === this.selectedId);
+  }
 
   onSelectUser(id: string) {
-    console.log("user clicked: " + id);
+    this.selectedId = id;
+    console.log("selectedId: " + this.selectedId)
   }
 }
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,12 @@
 import { Component, EventEmitter, Input, Output, } from '@angular/core';
 
+
+type User = {
+  id: string, 
+  name: string,
+  age: string
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,18 +15,20 @@ import { Component, EventEmitter, Input, Output, } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true})  name!: string;
-  @Input({required: true})  avatar!: string;
-  @Input({required: true})  id!: string;
+  @Input({required: true})  user!: {
+    id: string,
+    name: string,
+    avatar: string,
+  }
 
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath () {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser () {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 
 }
