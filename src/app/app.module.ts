8b27e5b89a8f7bef9bf940e20781f1d6cd5bd741
daumtto53 <daumtto53@gmail.com>
2025-01-19 16:16:31 +0900

    Using NgModules w/ Standalone and imports.
    * Standalone and Imports
    * NgModules
    * BrowserModule when Using NgModuels and Bootstrapping.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { UserComponent } from "./user/user.component";
+import { HeaderComponent } from "./header/header.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 @NgModule({
     declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, HeaderComponent, TasksComponent, UserComponent]
+    
 })
 export class AppModule {
 
